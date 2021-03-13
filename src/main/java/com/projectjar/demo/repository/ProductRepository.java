7f8498ac723692f3d374a37809c3b39b666be473
muhammet-sahin-ITU <muhammetsahin@itu.edu.tr>
2021-03-13 16:00:23 +0300

    updated

--- src/main/java/com/projectjar/demo/repository/ProductRepository.java
+++ src/main/java/com/projectjar/demo/repository/ProductRepository.java
@@ -2,7 +2,9 @@ package com.projectjar.demo.repository;
 
 import com.projectjar.demo.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository  extends JpaRepository<Product,Long> {
 
 }
--- src/main/java/com/projectjar/demo/service/ProductService.java
+++ src/main/java/com/projectjar/demo/service/ProductService.java
@@ -1,9 +1,10 @@
 package com.projectjar.demo.service;
 
 import com.projectjar.demo.entity.Product;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public interface ProductService {
 
     Product createProduct(Product product);
